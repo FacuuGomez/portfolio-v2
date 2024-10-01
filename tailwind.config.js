a19cbf43843e25a6f2db6615c27c93c4aa9f90fb
FacuuGomez <facuugomez67@gmail.com>
2024-10-01 16:58:25 -0300

    fix5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js}'],
+	content: ['./*.html', './*.js'], // Ajusta esta línea para que incluya tus archivos HTML y JS en la raíz
 	darkMode: 'class',
 	theme: {
 		extend: {},
